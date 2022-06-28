02565e23d10e8676afaa123a2e6bbc4a84571a84
Maciej Garncarski <60816511+MaciejGarncarski@users.noreply.github.com>
2022-06-28 21:55:37 +0200

    fix: bugfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
-import '../styles/tailwind.css';
-
 import type { AppProps } from 'next/app';
 import { QueryClientProvider, QueryClient, Hydrate } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { useRef } from 'react';
+import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = useRef(new QueryClient());
