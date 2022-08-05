96b9e66150030adf30c5947f1d83801ddebb86ea
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-08-05 22:16:31 +0200

    style: reset button text color

--- src/components/atoms/buttons/ResetButton.tsx
+++ src/components/atoms/buttons/ResetButton.tsx
@@ -15,7 +15,7 @@ export const ResetButton = ({ inputValue }: ResetButtonProps) => {
       whileFocus={{ scale: 0.8, borderRadius: '8px' }}
       disabled={inputValue === '' ? true : false}
       className={clsxm(
-        'hidden px-3 text-slate-200 transition-colors focus:text-slate-600 focus:outline-4 focus:outline-offset-2 enabled:hover:bg-slate-200 enabled:hover:text-slate-600 enabled:focus:bg-slate-200 disabled:cursor-not-allowed disabled:text-slate-300 sm:inline md:text-3xl'
+        'hidden px-3 text-slate-200 transition-colors focus:text-slate-600 focus:outline-4 focus:outline-offset-2 enabled:hover:bg-slate-200 enabled:hover:text-slate-600 enabled:focus:bg-slate-200 disabled:cursor-not-allowed disabled:text-slate-200 sm:inline md:text-3xl'
       )}
     >
       <IoMdClose />
