88ac3d5066e7b64a2493b6319d40ff85e225de30
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-08-12 18:23:52 +0200

    fix: add accessible name to button

--- src/components/atoms/buttons/ResetButton.tsx
+++ src/components/atoms/buttons/ResetButton.tsx
@@ -14,6 +14,7 @@ export const ResetButton = ({ inputValue }: ResetButtonProps) => {
       disabled={inputValue === '' ? true : false}
       className='hidden px-4 text-2xl text-slate-200 transition enabled:hover:bg-slate-500 enabled:focus:bg-slate-500 disabled:opacity-40 sm:inline'
       data-testid='reset-btn'
+      aria-label='reset'
     >
       <IoMdClose />
     </motion.button>
